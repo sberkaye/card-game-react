377dfc1ef5ef8726fb8c6e5ebdce5fd951f395b3
sberkaye <berkayergin99@gmail.com>
2021-04-07 23:51:20 +0300

    Problem in the state of CardContainer, can't properly fill an array with false values.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-const Card = ({ size }) => {
+const Card = ({ size, active, changeActive }) => {
+  const handleClick = () => {
+    changeActive(true);
+    console.log("clicked");
+  };
   return (
-    <div className={`card card-${size}`}>
+    <button
+      type="button"
+      onClick={handleClick}
+      className={`card card-${size} ${active ? `card--active` : ``}`}
+    >
       <div className="card__side card__side--front">SA</div>
       <div className="card__side card__side--back">AS</div>
-    </div>
+    </button>
   );
 };
 
 Card.propTypes = {
   size: PropTypes.number.isRequired,
+  active: PropTypes.bool.isRequired,
+  changeActive: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => {
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 import Card from "./Card";
 
 const CardContainer = ({ difficulty }) => {
+  const [active, setActive] = useState([...new Array(difficulty).fill(false)]);
+
+  useEffect(() => {
+    setActive([...new Array(difficulty).fill(false)]);
+  }, [difficulty]);
+
+  // function currying for setActive to make it easier to use
+  // in the child Card component
+  const changeActiveStatus = (index) => (status) => {
+    setActive([...active.slice(0, index), status, ...active.slice(index + 1)]);
+  };
+
   const generateCards = () => {
-    return Array.from(Array(parseInt(difficulty, 10))).map((card) => (
-      <Card size={1} key={`card-${card}`} />
+    return Array.from(Array(parseInt(difficulty, 10))).map((card, index) => (
+      <Card
+        active={active[index]}
+        changeActive={changeActiveStatus(index)}
+        size={1}
+        key={`card-${card}`}
+      />
     ));
   };
 
-  return <>{generateCards()}</>;
+  return (
+    <>
+      {generateCards()}
+      {console.log("difficulty in CardContainer :>> ", difficulty)}
+      {console.log("active: ", active)}
+      {console.log("active.length = ", active.length)}
+    </>
+  );
 };
 
 CardContainer.propTypes = {
